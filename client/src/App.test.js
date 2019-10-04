e5170927eca64682cd450d44c600948e3c0f2b08
Adam Payne <ajpayne238@gmail.com>
2019-10-04 12:16:17 -0400

    added 4 functions and 4 unit tests that work correctly

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -9,7 +9,29 @@ test('Adds Parameter a and b', () => {
   expect(actual).toBe(expected);
 });
 
+test('Subtracts Parameter a and b', () => {
+  let actual;
+  let expected;
+  actual = subtract(10, 5);
+  expected = 5;
+  expect(actual).toBe(expected);
+});
 
+test('Multiplies Parameter a and b', () => {
+  let actual;
+  let expected;
+  actual = multiply(5, 20);
+  expected = 100;
+  expect(actual).toBe(expected);
+});
+
+test('Divides Parameter a and b', () => {
+  let actual;
+  let expected;
+  actual = divide(40, 4);
+  expected = 10;
+  expect(actual).toBe(expected);
+});
 
 
 
